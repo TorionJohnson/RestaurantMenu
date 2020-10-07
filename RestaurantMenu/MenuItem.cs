fb57035171c0a7c27d6ba7064e3f28b4d478546c
Torion <teezus318@gmail.com>
2020-10-06 21:34:40 -0500

    RestaurantMenu:Classes added

---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        private double price;
+        private string description;
+        private string category;
+
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+ 
+        public MenuItem(double price, string description, string category)
+        {
+            this.price = price;
+            this.description = description;
+            this.category = category;       
+        }
+
+        public MenuItem() : this(0.0, "", "") { }
+    }
+}
